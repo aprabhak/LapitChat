dd34aa2dc87f5371a55bbbca46effccb1547695e
Avinash <aprabhak@purdue.edu>
2017-07-28 19:31:33 +0800

    Account settings page done. Offline functionality to be added later.

--- app/src/main/java/com/hfad/lapitchat/SettingsActivity.java
+++ app/src/main/java/com/hfad/lapitchat/SettingsActivity.java
@@ -1,10 +1,18 @@
 package com.hfad.lapitchat;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.net.Uri;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
@@ -12,6 +20,14 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+import com.squareup.picasso.Picasso;
+import com.theartofdev.edmodo.cropper.CropImage;
+import com.theartofdev.edmodo.cropper.CropImageView;
+
+import java.util.Random;
 
 import de.hdodenhof.circleimageview.CircleImageView;
 
@@ -24,6 +40,15 @@ public class SettingsActivity extends AppCompatActivity {
     private TextView mName;
     private TextView mStatus;
 
+    private Button mStatusBtn;
+    private Button mImageBtn;
+
+    private static final int GALLERY_PICK = 1;
+
+    private StorageReference mImageStorage;
+
+    private ProgressDialog mProgressDialog;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -32,6 +57,12 @@ public class SettingsActivity extends AppCompatActivity {
         mDisplayImage = (CircleImageView)findViewById(R.id.settings_image);
         mName = (TextView)findViewById(R.id.settings_display_name);
         mStatus = (TextView)findViewById(R.id.settings_status);
+
+        mStatusBtn = (Button)findViewById(R.id.settings_status_btn);
+
+        final ProgressDialog Dialog = new ProgressDialog(SettingsActivity.this);
+        Dialog.setMessage("Loading data");
+        Dialog.show();
 
         mCurrentUser = FirebaseAuth.getInstance().getCurrentUser();
         String current_uid = mCurrentUser.getUid();
@@ -47,6 +78,8 @@ public class SettingsActivity extends AppCompatActivity {
 
                 mName.setText(name);
                 mStatus.setText(status);
+                Picasso.with(SettingsActivity.this).load(image).into(mDisplayImage);
+                Dialog.dismiss();
             }
 
             @Override
@@ -54,5 +87,88 @@ public class SettingsActivity extends AppCompatActivity {
 
             }
         });
+
+        mStatusBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String status_value = mStatus.getText().toString();
+                Intent status_intent = new Intent(SettingsActivity.this,StatusActivity.class);
+                status_intent.putExtra("status_value",status_value);
+                startActivity(status_intent);
+            }
+        });
+
+        mImageBtn = (Button)findViewById(R.id.settings_image_btn);
+
+        mImageStorage = FirebaseStorage.getInstance().getReference();
+
+        mImageBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+               Intent galleryIntent = new Intent();
+                galleryIntent.setType("image/*");
+                galleryIntent.setAction(Intent.ACTION_GET_CONTENT);
+                startActivityForResult(Intent.createChooser(galleryIntent,"SELECT_IMAGE"),GALLERY_PICK);
+               /* CropImage.activity()
+                        .setGuidelines(CropImageView.Guidelines.ON)
+                        .start(SettingsActivity.this); */
+            }
+        });
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == GALLERY_PICK && resultCode == RESULT_OK) {
+            Uri imageUri = data.getData();
+            CropImage.activity(imageUri)
+                    .setAspectRatio(1,1)
+                    .start(this);
+
+
+            //Toast.makeText(SettingsActivity.this, imageUri, Toast.LENGTH_LONG).show();
+        }
+        if (requestCode == CropImage.CROP_IMAGE_ACTIVITY_REQUEST_CODE) {
+            CropImage.ActivityResult result = CropImage.getActivityResult(data);
+            if (resultCode == RESULT_OK) {
+
+                mProgressDialog = new ProgressDialog(SettingsActivity.this);
+                mProgressDialog.setTitle("Uploading Image...");
+                mProgressDialog.setMessage("Please wait while we upload and process the image");
+                mProgressDialog.setCanceledOnTouchOutside(false);
+                mProgressDialog.show();
+
+                Uri resultUri = result.getUri(); //Uri is like a FILE. In this case it is image.
+                String current_user_id = mCurrentUser.getUid();
+                StorageReference filepath = mImageStorage.child("profile_images").child(current_user_id+".jpg");
+                filepath.putFile(resultUri).addOnCompleteListener(new OnCompleteListener<UploadTask.TaskSnapshot>() { //add to storage
+                    @Override
+                    public void onComplete(@NonNull Task<UploadTask.TaskSnapshot> task) {
+                        if (task.isSuccessful()) {
+                            //Toast.makeText(SettingsActivity.this, "working", Toast.LENGTH_LONG).show();
+                            @SuppressWarnings("VisibleForTests")
+                            String download_url = task.getResult().getDownloadUrl().toString();
+                            mUserDatabase.child("image").setValue(download_url).addOnCompleteListener(new OnCompleteListener<Void>() { //add to database
+                                @Override
+                                public void onComplete(@NonNull Task<Void> task) {
+                                    if (task.isSuccessful()) {
+                                        mProgressDialog.dismiss();
+                                        Toast.makeText(SettingsActivity.this, "Success uploading", Toast.LENGTH_LONG).show();
+                                    }
+                                }
+                            }); //mUserDatabase already pointing to current user from before.
+                        } else {
+                            Toast.makeText(SettingsActivity.this, "error in uploading", Toast.LENGTH_LONG).show();
+                            mProgressDialog.dismiss();
+                        }
+                    }
+                });
+
+            } else if (resultCode == CropImage.CROP_IMAGE_ACTIVITY_RESULT_ERROR_CODE) {
+                Exception error = result.getError();
+            }
+        }
     }
 }
