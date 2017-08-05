ef92302e11a41088df04bc7ee739f510eb34b79a
Avinash <aprabhak@purdue.edu>
2017-08-05 13:58:40 +0800

    receive and show messages on Chat page.

---
+++ app/src/main/java/com/hfad/lapitchat/MessageAdapter.java
@@ -0,0 +1,52 @@
+package com.hfad.lapitchat;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.List;
+
+import de.hdodenhof.circleimageview.CircleImageView;
+
+/**
+ * Created by avina on 8/5/2017.
+ */
+
+public class MessageAdapter extends RecyclerView.Adapter<MessageAdapter.MessageViewHolder> {
+    private List<Messages> mMessageList;
+
+    public MessageAdapter(List<Messages> mMessageList) {
+        this.mMessageList = mMessageList;
+    }
+
+    @Override
+    public MessageViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View v = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.message_single_layout,parent,false);
+        return new MessageViewHolder(v);
+    }
+
+    public class MessageViewHolder extends RecyclerView.ViewHolder {
+        public TextView messageText;
+        public CircleImageView profileImage;
+
+        public MessageViewHolder(View view) {
+            super(view);
+            messageText = (TextView)view.findViewById(R.id.message_text_layout);
+            profileImage = (CircleImageView)view.findViewById(R.id.message_profile_layout);
+        }
+    }
+
+    @Override
+    public void onBindViewHolder(MessageViewHolder viewHolder,int i) {
+        Messages c = mMessageList.get(i);
+        viewHolder.messageText.setText(c.getMessage());
+    }
+
+    @Override
+    public int getItemCount() {
+        return mMessageList.size();
+    }
+}
---
+++ app/src/main/java/com/hfad/lapitchat/Messages.java
@@ -0,0 +1,55 @@
+package com.hfad.lapitchat;
+
+/**
+ * Created by avina on 8/5/2017.
+ */
+
+public class Messages {
+
+    private String message, type;
+    private long time;
+    private boolean seen;
+
+    public Messages(String message, String type, long time, boolean seen) {
+        this.message = message;
+        this.type = type;
+        this.time = time;
+        this.seen = seen;
+    }
+
+    public Messages() {
+
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public boolean isSeen() {
+        return seen;
+    }
+
+    public void setSeen(boolean seen) {
+        this.seen = seen;
+    }
+
+    public long getTime() {
+        return time;
+    }
+
+    public void setTime(long time) {
+        this.time = time;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+}
